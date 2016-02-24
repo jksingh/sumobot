72ec01cdf6dc2ff217bd0969fee7750c2841ebd2
Jitendra Singh <jitendra@sumologic.com>
2016-02-24 19:32:32 +0530

    cleanup GPIO

--- webapp/server.py
+++ webapp/server.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
 from flask import Flask,render_template, abort
-from bot import Bot
 from flask import request, Response
 
+from bot import Bot
+import RPi.GPIO as GPIO
+
+
 import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -12,26 +16,26 @@ bot = Bot()
 
 @app.route('/robot/api/v1.0/stop', methods=['GET'])
 def bot_stop():
-	bot.stop()
-	return Response({}, status=200, mimetype='application/json')
+    bot.stop()
+    return Response({}, status=200, mimetype='application/json')
 
 @app.route('/robot/api/v1.0/setFineMovement', methods=['GET'])
 def setFineMovement():
-	bot.setFineMovement()
-	return Response({}, status=200, mimetype='application/json')
+    bot.setFineMovement()
+    return Response({}, status=200, mimetype='application/json')
 
 @app.route('/robot/api/v1.0/setCoarseMovement', methods=['GET'])
 def setCoarseMovement():
-	bot.setCoarseMovement()
-	return Response({}, status=200, mimetype='application/json')
+    bot.setCoarseMovement()
+    return Response({}, status=200, mimetype='application/json')
 
 @app.route('/robot/api/v1.0/move', methods=['POST'])
 def run_command():
-	if not request.form or not 'ch' in request.form:
-		abort(400)
-	ch = request.form['ch']
-	logging.debug('Received command %s' %ch)
-	bot.move(ch)
+    if not request.form or not 'ch' in request.form:
+        abort(400)
+    ch = request.form['ch']
+    app.logger.info('Received command %s' %ch)
+    bot.move(ch)
     resp = Response({}, status=200, mimetype='application/json')
     return resp
 
@@ -40,10 +44,16 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-	logging.basicConfig(level=logging.DEBUG)
 
-	try:
-		app.run(host='0.0.0.0', port=8080, debug=True)
-		bot.start()
-	finally:
-		GPIO.cleanup()
+    formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler = RotatingFileHandler('log/bot.log', maxBytes=10000000, backupCount=5)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
+    try:
+        bot.start()
+        app.run(host='0.0.0.0', port=8080, debug=False)
+    finally:
+        bot.stop()
+        GPIO.cleanup()
