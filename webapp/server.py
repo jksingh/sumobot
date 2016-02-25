00587cea881e8943a39e70413160546b2f5e961c
Ritesh Kumar <ritesh@sumologic.com>
2016-02-25 14:01:50 +0530

    minor changes

--- webapp/server.py
+++ webapp/server.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
         bot = Bot(logger = app.logger)
         bot.start()
         app.logger.info('***********STARTED SUMOBOT***********')
-        context = ('./hangout.key', './hangout.cert')
+        context = ('./hangout.cert', './hangout.key')
         app.run(host='0.0.0.0', port=8080, debug=False, ssl_context=context)
     finally:
         bot.stop()
