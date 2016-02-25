f19bcb273fcba5516f4f66075c3cfd88952d58e1
Jitendra Singh <jitendra@sumologic.com>
2016-02-25 15:36:12 +0530

    certificates added

--- webapp/server.py
+++ webapp/server.py
@@ -2,6 +2,8 @@
 
 from flask import Flask,render_template, abort
 from flask import request, Response
+from werkzeug.serving import run_simple
+import ssl
 
 from bot import Bot
 import RPi.GPIO as GPIO
@@ -61,8 +63,14 @@ if __name__ == '__main__':
         bot = Bot(logger = app.logger)
         bot.start()
         app.logger.info('***********STARTED SUMOBOT***********')
-        context = ('./hangout.cert', './hangout.key')
-        app.run(host='0.0.0.0', port=8080, debug=False, ssl_context="adhoc")
+        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        context.load_cert_chain('server.crt', 'server.key')
+        run_simple('0.0.0.0', 8443, app, ssl_context=context)
+        #context.use_privatekey_file('/home/pi/sumobot/webapp/server.key')
+        #context.use_certificate_file('/home/pi/sumobot/webapp/server.crt')
+        #app.run(use_reloader=True, host='0.0.0.0',port=8080,ssl_context = context, debug=False)
+        #context = ('server.crt', 'server.key')
+        #app.run(host='0.0.0.0', port=8080, debug=False, ssl_context=context)
     finally:
         bot.stop()
         GPIO.cleanup()
